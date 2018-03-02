817f1a183c19a2efff1609b408d57221ede7b354
lmz2050 <295925348@qq.com>
2018-03-02 18:36:03 +0800

    spring cloud

---
+++ cloud/service-feign/src/main/java/cn/lmz/mike/service/feign/SchedualServiceHiHystric.java
@@ -0,0 +1,11 @@
+package cn.lmz.mike.service.feign;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SchedualServiceHiHystric implements SchedualServiceHi {
+    @Override
+    public String sayHiFromClientOne(String name) {
+        return "sorry "+name;
+    }
+}
